a5d2b1f747951f367d4ae31d454effa46d6b3c74
Faisal Khalid <faisalkhalid@Faisals-MacBook-Pro.local>
2020-05-27 22:07:19 -0500

    Index set up

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!")
+  }
+})
+client.login("NzE1Mjg1ODA3NDY3OTg2OTU1.Xs8c2A.3gbOcRSb2ivigi4DBUboUHJVcIM")
