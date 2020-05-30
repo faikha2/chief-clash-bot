54c7e42d544966904e49ca14370a9e3cdcfc504d
Faisal Khalid <faisalkhalid@Faisals-MacBook-Pro.local>
2020-05-30 12:39:52 -0500

    Compressed code a bit

--- commands/API_call.js
+++ commands/API_call.js
@@ -1,9 +1,24 @@
-// Create a way to get API info. We're using the request method
+/**
+ *  Contains the code for the main API call to the Clash of Clan (mobile game) server. Sends a GET request through
+ *  a Fixe proxy server (to allow for a static IP and then) and gets data for members of a specific in-game clan. The 
+ *  incoming data is stored as a JSON file for easy access and updating.
+ * 
+ *  Once the file is created, another JSON file is created to hold all the 'strike' information for each member of the clan. Will
+ *  most likely be run daily.
+ * 
+ *  Relevent functions: 
+ *    @call_api()
+ *    @create_strike_data()
+ *    @update_data()
+ * 
+ *  File version: 1.0
+ */
+
+// Permissions/access to other modules and libraries
 const rp = require('request-promise');
-// Get access to the file system
 const fs = require('fs');
 
-// Let's the request-promise module know we are routhing traffic through a proxy server
+// Sets up proxy server
 const fixieRequest = rp.defaults({'proxy': process.env.FIXIE_URL});
 
 // URL and my token of the target site
@@ -19,7 +34,13 @@ var options = {
     json: true // Automatically parses the JSON string in the response
 };
 
-// Actual API call done here through a request promise
+/*
+  Uses NodeJs request-promise to make a GET request to the Clash of Clans server. It recieves the data as a JSON string and 
+  saves it into a file.
+
+  Parameters: None
+  Relevant files: user_data.json
+*/
 function call_api () {
     // Uses our proxy
     fixieRequest(options)
@@ -30,87 +51,88 @@ function call_api () {
                 if (err) throw err;
                 console.log('Data written to file');
             });
-
-            // Return the response
             return response;
         })
         .catch(function (err) {
-            // API call failed...
+            // API call failed
             console.log("Error in API_call");
-
-            // Return's nothing if there's an error
             return null;
         });
-} // End call_api
+} 
 
-// Parses through JSON file to get names and create our json data
+/*
+  Takes our previously created user_data (from the API call) and extracts the usernames (clan member names). The list of usernames 
+  is then used to create a new file to handle all of the bot's strike information.
+
+  Parameters: None
+  Relevant files: user_strikes_data
+*/
 function create_strike_data() {
 
-    // Opens up the json file and reads data into an object variable
-    var obj;
-    fs.readFile('user_data.json', 'utf8', function (err, data) {
+    // Easy way to both open and parse a JSON file
+    var user_data = require('../user_data.json');
 
-        // Basic error handling
+    // Extracts the clanmate's usernames from the JSON file
+    var usernames = []
+    for (key in user_data.items) {
+        usernames.push(user_data.items[key].name);
+    }
+
+    // Creates the nested array that will eventually be turned into a JSON file
+    var txt = "";
+    var json_strike_data = [];
+    for (var i = 0; i < usernames.length; i++) {
+
+        // The format that our data is pushed into the array
+        json_strike_data.push(
+            {
+                name: usernames[i], 
+                strikes: false, 
+                num_strikes: 0,
+                reasons: 
+                [
+                    { reason_1: txt, 
+                      reason_2: txt, 
+                      reason_3: txt
+                    }
+                ]
+            });
+    }
+
+    // Takes that strike data array we just made and finally turns it into a JSON opject
+    var data = JSON.stringify(json_strike_data);
+
+    // Writes that JSON object into a new file
+    fs.writeFile('user_strikes_data.json', data, (err) => {
         if (err) throw err;
-
-        // Converts the file into a JSON object
-        obj = JSON.parse(data);
-
-        // Place holder for names from JSON object
-        var usernames = [];
-        // Loops through the JSON object and accesses clan member's names and pushes it into an array
-        for (key in obj.items) {
-            // console.log(obj.items[key].name)
-            usernames.push(obj.items[key].name);
-        }
-
-        // Takes the array of names and makes a new array of strike data
-        var txt = "";
-        var json_strike_data = [];
-        for (var i = 0; i < usernames.length; i++) {
-            json_strike_data.push({name: usernames[i], strikes: false, num_strikes: 0, reasons: [{reason_1: txt, reason_2: txt, reason_3: txt}]});
-        }
-
-        // Takes that strike data array and turns it into a JSON version
-        var data = JSON.stringify(json_strike_data);
-
-        // Writes that into a new file
-        fs.writeFile('user_strikes_data.json', data, (err) => {
-            if (err) throw err;
-            console.log('Data written to file');
-        });
+        console.log('Data written to file');
     });
-} // End create_strike_data
+}
 
+/*
+  The point of this function is to go through the user data file (from API call) and the user strike data (made by extracting 
+  usernames) and compare to see if new members were added or if old members need to be removed.
+
+  Parameters: None
+  Relevent files: user_data.json, user_strikes_data,json
+*/
 function update_data() {
 
-    // Place holder for names from JSON object
-    var usernames = [];
+    // Easy way to both open and parse a JSON file
+    var user_data = require('../user_data.json');
+    
+    // Extracts the clanmate's usernames from the JSON file
+    var usernames = []
+    for (key in user_data.items) {
+        usernames.push(user_data.items[key].name);
+    }
 
-    // Opens up the json file and reads data into an object variable
-    var obj;
-    fs.readFileSync('user_data.json', 'utf8', function (err, data) {
+    console.log(usernames);
+} 
 
-        // Basic error handling
-        if (err) throw err;
-
-        // Converts the file into a JSON object
-        obj = JSON.parse(data);
-
-        // Loops through the JSON object and accesses clan member's names and pushes it into an array
-        for (key in obj.items) {
-            // console.log(obj.items[key].name)
-            usernames.push(obj.items[key].name);
-        }
-
-        console.log('In: ' + usernames);
-
-    });
-
-    console.log('OUT: ' + usernames);
-
-} // End update_data
-
+/*
+  The functions that are exported from this module (meaning they can be used in other files as well)
+*/
 module.exports = {
     call_api : call_api, 
     create_strike_data : create_strike_data,
