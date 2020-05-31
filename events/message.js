eabbba661340111fdcaf76f5f0dc940e950cc334
Faisal Khalid <faisalkhalid@Faisals-MacBook-Pro.local>
2020-05-30 20:48:03 -0500

    Added schedule support

--- events/message.js
+++ events/message.js
@@ -1,5 +1,6 @@
 // Allows us to call the API functions
 var api = require('../commands/API_call');
+var schedule = require('node-schedule');
 
 module.exports = (client, msg) => {
 
@@ -7,5 +8,19 @@ module.exports = (client, msg) => {
        api.update_data();
     }
 
+    if (msg.content === "Ya") {
+        console.log("THIS IS A TEST");
+        msg.reply("Yo");
+    }
+
+    // var j = schedule.scheduleJob('*/5 * * * *', function(){
+    //     console.log('The answer to life, the universe, and everything!');
+    //     msg.reply("This should happen every 5 minutes");
+    // });
+
+    var j = schedule.scheduleJob('0 0 * * *', function(){
+        console.log('The answer to life, the universe, and everything!');
+        msg.reply("This should happen at midnight every night");
+    });
 
 }
