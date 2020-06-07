07cce85ed49e53a8a30ef8597fdb78a7ccd09924
ImFaisalKhalid <faisalkhalid246@gmail.com>
2020-06-06 20:25:48 -0500

    Removed .DS_Store file locally

--- events/message.js
+++ events/message.js
@@ -1,6 +1,6 @@
 // Allows us to call the API functions
 var api = require('../commands/API_call');
-var schedule = require('node-schedule');
+// var schedule = require('node-schedule');
 
 module.exports = (client, msg) => {
 
@@ -18,9 +18,9 @@ module.exports = (client, msg) => {
     //     msg.reply("This should happen every 5 minutes");
     // });
 
-    var j = schedule.scheduleJob('0 0 * * *', function(){
-        console.log('The answer to life, the universe, and everything!');
-        msg.reply("This should happen at midnight every night");
-    });
+    // var j = schedule.scheduleJob('0 0 * * *', function(){
+    //     console.log('The answer to life, the universe, and everything!');
+    //     msg.reply("This should happen at midnight every night");
+    // });
 
 }
