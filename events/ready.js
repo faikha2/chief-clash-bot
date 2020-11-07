d25f760e3840699df9529d5b60f4d90a50f33081
root <faisalkhalid246@gmail.com>
2020-11-06 20:37:26 -0600

    API now updates daily

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 var schedule = require('node-schedule');
 const { MessageFlags } = require('discord.js');
 var display = require('../commands/display');
+var api = require('../commands/API_call');
 
 require('log-timestamp');
 
@@ -9,6 +10,11 @@ module.exports = client => {
 
     var j = schedule.scheduleJob('0 0 7 * * *', function(){
         const channel = client.channels.cache.get('717213839267201061');
+
+
+        api.call_api()
+        api.update_data()
+
         display.display_names(client)
 
         channel.send('24 hour confirmation!');
